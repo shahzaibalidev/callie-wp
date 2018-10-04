db11627254625437b26c48454466b39c3ebe1091
Shahzaib Ali <shahzaibali.dev@gmail.com>
2018-10-04 20:37:45 +0500

    adding the_tags() functions.

--- single.php
+++ single.php
@@ -68,11 +68,7 @@
 					<div class="section-row">
 						<div class="post-tags">
 							<ul>
-								<li>TAGS:</li>
-								<li><a href="#">Social</a></li>
-								<li><a href="#">Lifestyle</a></li>
-								<li><a href="#">Fashion</a></li>
-								<li><a href="#">Health</a></li>
+								<?php the_tags( 'Tags: ',', '); ?>
 							</ul>
 						</div>
 					</div>
