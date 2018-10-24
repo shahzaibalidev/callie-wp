0885875a5175f429861020dca9f501728837c4db
Shahzaib Ali <shahzaibali.dev@gmail.com>
2018-10-24 22:06:20 +0500

    admin panel general page
    added header logo text input field

--- adminpanel/function-admin.php
+++ adminpanel/function-admin.php
@@ -38,8 +38,8 @@
 			'admin_panel_create_settings_page'	//Callback Function.
 		);
 
-		//06: Adding Admin Panel custom settings function.
-		add_action('admin_init', 'admin_panel_custom_settings');
+		//06: Adding Admin Panel custom general settings function.
+		add_action('admin_init', 'admin_panel_custom_general');
 	}
 	//04: Callback function of 01 & 02 Admin Panel  MAIN page.
 	function admin_panel_create_page(){
@@ -55,8 +55,39 @@
 
 	}
 
-	//07: Callback function of 06 Admin Panel custom settings.
-	function admin_panel_custom_settings(){
+	//07: Callback function of 06 Admin Panel custom general settings.
+	function admin_panel_custom_general(){
+		//Using Hooks apis.
+		register_setting(
+			'admin-panel-general-group', //Option Group.
+			'header_logo' //Option Name.
+		);
+		//08: Settings Section.
+		add_settings_section(
+			'admin-panel-general-options', //ID.
+			'General Options', //Title.
+			'admin_panel_general_options', //Callback fuction.
+			'admin_panel' //page.
+		);
+		//10: Adding setting fields.
+		add_settings_field(
+			'header-logo', //ID.
+			'Header Logo', //Title.
+			'header_logo', //Callback function.
+			'admin_panel', //page.
+			'admin-panel-general-options' //section.
+			//args or Array.
+		);
+	}
 
+	//09: Callback function of 08 admin_panel_general_options.
+	function admin_panel_general_options(){
+		echo 'Customize General Settings';
+	}
+
+	//11: Callback function of 10 general_logo.
+	function header_logo(){
+		$headerLogo = esc_attr( get_option( 'header_logo' ) );
+		echo '<input type="text" name="header_logo" value="'.$headerLogo.'" placeholder="Header Logo"/>';
 	}
 ?>
--- adminpanel/includes/admin_panel_general.php
+++ adminpanel/includes/admin_panel_general.php
@@ -1,4 +1,7 @@
-<h1>General</h1>
-<form action="" method="post">
-	
+<h1>Admin Panel General</h1>
+<?php settings_errors(); ?>
+<form method="post" action="options.php">
+	<?php settings_fields('admin-panel-general-group'); ?>
+	<?php do_settings_sections('admin_panel'); ?>
+	<?php submit_button(); ?>
 </form>
