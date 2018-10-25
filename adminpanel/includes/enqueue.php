82a231202e310fd5627e3ae63a02180f10dc0eb9
Shahzaib Ali <shahzaibali.dev@gmail.com>
2018-10-25 21:45:06 +0500

    Admin Panel CSS
    Admin Panel CSS

--- adminpanel/includes/enqueue.php
+++ adminpanel/includes/enqueue.php
@@ -12,5 +12,8 @@ function admin_panel_load_scripts( $hook ){
 	}
 	wp_register_style( 'admin_panel', get_template_directory_uri() . '/adminpanel/assets/css/admin-panel.css', array(), '1.0.0', 'all' );
 	wp_enqueue_style( 'admin_panel' );
+
+	wp_register_script( 'admin-panel-script', get_template_directory_uri() . '/adminpanel/assets/js/admin.panel.js', array('jquery'), '1.0.0', true);
+	wp_enqueue_script( 'admin-panel-script' );
 }
 add_action( 'admin_enqueue_scripts', 'admin_panel_load_scripts');
--- adminpanel/includes/pages/admin_panel_general.php
+++ adminpanel/includes/pages/admin_panel_general.php
@@ -1,6 +1,15 @@
 <h1>Admin Panel General</h1>
 <?php settings_errors(); ?>
-<form method="post" action="options.php">
+
+<div class="general-preview">
+	<div class="logo-preview">
+		<div class="icons-wrapper">
+
+		</div>
+	</div>
+</div>
+
+<form method="post" action="options.php" class="general-form">
 	<?php settings_fields('admin-panel-general-group'); ?>
 	<?php do_settings_sections('admin_panel'); ?>
 	<?php submit_button(); ?>
